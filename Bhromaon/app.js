c01dfcb4be03b3f668ec99ea0ada2b2e55ce212b
AntonDubinchuk <sampanton085@gmail.com>
2022-09-01 03:05:49 +0300

    main

---
+++ Bhromaon/app.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("nav-links_wrapper");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
